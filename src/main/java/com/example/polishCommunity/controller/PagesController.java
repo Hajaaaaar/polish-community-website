625927900cf60f912e4501fd93ddcfd450a038eb
c24004894 <MoraleV@cardiff.ac.uk>
2024-11-25 13:50:43 +0000

    Resolved Page Controller conflict

--- src/main/java/com/example/polishCommunity/controller/PagesController.java
+++ src/main/java/com/example/polishCommunity/controller/PagesController.java
@@ -11,20 +11,15 @@ public class PagesController {
     public String home(Model model) {
         return "Pages/home";
     }
+
     @GetMapping("/EventsPage")
     public String EventsPage(Model model) {
         return "Pages/EventsPage";
     }
-//    @GetMapping("/home")
-//    public String home(Model model) {
-//        model.addAttribute("lang", "en"); // Default language (or retrieve it dynamically)
-//        return "Pages/home";
-//    }
-<<<<<<< HEAD
+
     @GetMapping("/home/contact")
     public String contact(Model model) {
     return "Pages/contactsPage";
 }
-=======
->>>>>>> 0c72edfcd7d2e445a2461a7291cc563bce2751f1
+
 }
