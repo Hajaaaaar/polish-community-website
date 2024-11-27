d9fdd4721c8aea1200ec58a6c8adc0748f1d9821
Mohamed <sheekhms@cardiff.ac.uk>
2024-11-27 12:19:48 +0000

    fixed the file structures

--- src/main/java/com/repository /WorkInformationRepositry.java
+++
@@ -1,9 +0,0 @@
-package com.workpage.repository;
-
-import com.yourpackage.model.WorkInformation;
-import java.util.List;
-
-public interface WorkInformationRepository {
-    void save(WorkInformation workInfo);  // Save a new work info record
-    List<WorkInformation> getAll();      // Fetch all work records
-}
