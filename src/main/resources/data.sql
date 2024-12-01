058061c521e2e8747564503f3d5d52a351c0da8b
Hajar Nefaa <nefaah@cardiff.ac.uk>
2024-12-01 23:02:15 +0000

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,12 @@
 # delete
 # from users;
+#
+# insert into users (id, name, surname, email, password_hash)
+# values (1, 'hajar', 'nefaa', 'hajarnefaa@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m');
+# insert into users (id, name, surname, email, password_hash)
+# values (2, 'sara', 'Doe', 'sara@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m');
+#
 
-insert into users (id, name, surname, email, password_hash)
-values (1, 'hajar', 'nefaa', 'hajarnefaa@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m');
-values (2, 'sara', 'Doe', 'sara@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m');
 
 
 INSERT INTO users (id, name, surname, email, password_hash)
