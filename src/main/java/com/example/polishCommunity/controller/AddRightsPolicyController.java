805bc8c828bf6a76514e685b7b1e124459825343
c24004894 <MoraleV@cardiff.ac.uk>
2024-12-05 14:36:10 +0000

    Working on new policy mvc

--- src/main/java/com/example/polishCommunity/controller/AddRightsPolicyController.java
+++ src/main/java/com/example/polishCommunity/controller/AddRightsPolicyController.java
@@ -1,8 +1,10 @@
 package com.example.polishCommunity.controller;
 
+import com.example.polishCommunity.model.CreatePolicy;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,5 +15,11 @@ public class AddRightsPolicyController {
         ModelAndView modelAndView=new ModelAndView("Pages/createRightsPolicy");
         return modelAndView;
     }
+
+    @PostMapping("addNewPolicy")
+    public ModelAndView submitForm(CreatePolicy createPolicy){
+        ModelAndView mav=new ModelAndView("redirect:Pages/createRightsPolicy");
+        return mav;
     }
+}
 
