2c1e51ef695437f6bf3cc6decc78cab22a4edcab
Hajar Nefaa <Nefaah@cardiff.ac.uk>
2024-12-07 00:15:23 +0000

    changed JPA to JDBC for both pages

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,11 @@
-delete from users;
+# delete from users;
 
-insert into users (id, name, surname, email, password_hash, role)
-values (1, 'hajar', 'nefaa', 'hajarnefaa@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m', 'User');
-insert into users (id, name, surname, email, password_hash)
-values (2, 'sara', 'Doe', 'sara@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m');
-
-INSERT INTO users (id, name, surname, email, password_hash, role)
-SELECT 1, 'hajar', 'nefaa', 'hajarnefaa@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m', 'User'
+INSERT INTO users (id, name, surname, email, password_hash)
+SELECT 1, 'hajar', 'nefaa', 'hajarnefaa@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = 1);
 
-INSERT INTO users (id, name, surname, email, password_hash, role)
-SELECT 2, 'sara', 'Doe', 'sara@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m', 'User'
+INSERT INTO users (id, name, surname, email, password_hash)
+SELECT 2, 'sara', 'Doe', 'sara@gmail.com', '$2a$10$4OfOn5s5Dh8XlVrI1jN0f.BX0LDb8CYsdTXSdPW.25l6KZ3K/zw3m'
 WHERE NOT EXISTS (SELECT 1 FROM users WHERE id = 2);
 
 DELETE FROM news;
