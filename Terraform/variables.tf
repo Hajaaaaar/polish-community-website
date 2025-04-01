4ad805722a627c4acf46b3cec4a0fef198ceb749
Hajar Nefaa <Nefaah@cardiff.ac.uk>
2025-04-01 21:29:24 +0100

    terraform

---
+++ Terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "flavor" { default = "m1.large" }
+variable "image" { default = "Rocky Linux 9 20220830" }
+variable "name1" { default = "RockyMatrix" }
+
+variable "keypair" { default = "openstackk" } # you may need to change this
+variable "network" { default = "openstack1_network" }   # you need to change this
+
+variable "pool" { default = "cscloud_private_floating" }
+variable "server1_script" { default = "./project.sh" }
+variable "security_description" { default = "Terraform security group" }
+variable "security_name" { default = "tf_project_security" }
